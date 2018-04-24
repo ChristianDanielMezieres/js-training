acdeb1c99c88ac404b6e890697c7ac7d2c2453c9
Chrys <chrys.wcs@gmail.com>
2018-04-24 18:10:17 +0200

    TTD 4

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -7,11 +7,21 @@
  * and return capitalize each words: "How are you ?" -> "How Are You ?"
  *
  */
+function jadenCase(phrase) {
+    var words = phrase.split(' ');
+    for (var i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].substr(1).toLowerCase();
+    } return words.join(' ');
+}
 
 
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.fail('You must write your own tests')
+assert.strictEqual(typeof jadenCase, 'function')
+assert.strictEqual(jadenCase.length, 1)
+assert.strictEqual(jadenCase('oh my gosh'), 'Oh My Gosh')
+assert.strictEqual(jadenCase('v'), 'V')
+
 // End of tests */
